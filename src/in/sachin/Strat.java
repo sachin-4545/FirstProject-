d60a543281f6b171bb7bed47af79cdbf26e25da9
Anikt Ghuge <ghugeaniket32@gmail.com>
2024-09-25 18:20:30 +0530

    first commit

---
+++ src/in/sachin/Strat.java
@@ -0,0 +1,16 @@
+package in.sachin;
+
+public class Strat {
+
+	public static void main(String[] args) {
+
+		String x = "sucess";
+		if (x == "fail") {
+			System.out.println("come");
+
+		}
+		else {
+			System.out.println(" work on it");
+		}
+	}
+}
